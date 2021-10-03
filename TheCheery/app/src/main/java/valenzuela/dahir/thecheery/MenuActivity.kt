5b03d12dfb357a5d9228bdefd9502a5c826fa0e1
dahir <dahirvalenzuela@gmail.com>
2021-10-02 21:57:30 -0700

    agregar platillos y crearcion del apk
    se finalizo el proyecto y se creo el apk de la aplicacion

--- TheCheery/app/src/main/java/valenzuela/dahir/thecheery/MenuActivity.kt
+++ TheCheery/app/src/main/java/valenzuela/dahir/thecheery/MenuActivity.kt
@@ -1,11 +1,20 @@
 package valenzuela.dahir.thecheery
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
+
+        var btncold: Button = findViewById(R.id.button_cold_drinks) as Button
+
+        btncold.setOnClickListener{
+            var intent: Intent = Intent(this,ProductosActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ TheCheery/app/src/main/java/valenzuela/dahir/thecheery/Product.kt
@@ -0,0 +1,8 @@
+package valenzuela.dahir.thecheery
+
+class Product(
+    var name: String,
+    var image: Int,
+    var description: String,
+    var price: Int
+)
